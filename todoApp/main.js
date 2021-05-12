e13bf5b1a93919277817f47152f6226ae800ab77
yana-gi <yanagisawa.hika@gmail.com>
2021-05-12 21:11:42 +0900

    保存処理実装

--- todoApp/main.js
+++ todoApp/main.js
@@ -1,8 +1,48 @@
-const app = new Vue({
+var STORAGE_KEY = 'todos_storage'
+var todoStorage = {
+    fetch: function() {
+        var todos = JSON.parse(
+            localStorage.getItem(STORAGE_KEY) || '[]'
+        )
+        todos.forEach(function(todo, index) {
+            todo.id = index
+        })
+        todoStorage.uid = todos.length
+        return todos
+    },
+    save: function(todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    }
+}
+
+var app = new Vue({
     el: '#app',
     data: {
-
+        todos: []
+    },
+    watch: {
+        todos: {
+            handler: function (todos) {
+                todoStorage.save(todos)
+            },
+            deep: true
+        }
+    },
+    created() {
+        this.todos = todoStorage.fetch()
     },
     methods: {
+        doAdd: function(event, value) {
+            var comment = this.$refs.comment
+            if (!comment.value.length) {
+                return
+            }
+            this.todos.push({
+                id: todoStorage.uid++,
+                comment: comment.value,
+                state: 0
+            })
+            comment.value = ''
+        }
     }
 })
