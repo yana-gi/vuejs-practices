2979914286b7455243760b784ba1e9d2255d4999
yana-gi <yanagisawa.hika@gmail.com>
2021-05-13 18:01:43 +0900

    lintなど修正

--- todoApp/main.js
+++ todoApp/main.js
@@ -2,22 +2,22 @@
 
 var STORAGE_KEY = 'todos_storage'
 var todoStorage = {
-    fetch: function() {
+    fetch: function () {
         var todos = JSON.parse(
             localStorage.getItem(STORAGE_KEY) || '[]'
         )
-        todos.forEach(function(todo, index) {
+        todos.forEach(function (todo, index) {
             todo.id = index
         })
         todoStorage.uid = todos.length
         return todos
     },
-    save: function(todos) {
+    save: function (todos) {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
     }
 }
 
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         todos: []
@@ -34,8 +34,8 @@ var app = new Vue({
         this.todos = todoStorage.fetch()
     },
     methods: {
-        doAdd: function(event, value) {
-            var comment = this.$refs.comment
+        doAdd: function (event, value) {
+            const comment = this.$refs.comment;
             if (!comment.value.length) {
                 return
             }
@@ -46,14 +46,8 @@ var app = new Vue({
             })
             comment.value = ''
         },
-        toOnEditMode: function(item) {
-            item.editMode = true
-        },
-        toOffEditMode: function(item) {
-            item.editMode = false
-        },
         doChangeComment: function (item, index) {
-            var comment = this.$refs.newComment[index]
+            const comment = this.$refs.newComment[index]
             if (!comment.value.length) {
                 item.editMode = false
                 return
@@ -62,8 +56,8 @@ var app = new Vue({
             item.editMode = false
         },
         doRemove: function (item) {
-            var index = this.todos.indexOf(item)
-            this.todos.splice(index,1)
+            const index = this.todos.indexOf(item)
+            this.todos.splice(index, 1)
         }
     }
 })
